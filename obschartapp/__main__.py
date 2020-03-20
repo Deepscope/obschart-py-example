2bef2f1076326ab8a564d4aeadef0bbb6b4bd2c3
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2020-03-21 00:32:31 +0300

    feat: upgrade to obschart 0.11.0

--- obschartapp/__main__.py
+++ obschartapp/__main__.py
@@ -13,11 +13,11 @@ client = ObschartClient(token)
 print("App server running...")
 
 
-async def on_response(response):
+async def on_response(response: ProgramTrackActionResponse):
     unit = response._data["response"]["steps"][0]["fields"][1]
 
     if unit == "1":
-        feedback = response.set_feedback(
+        feedback = await response.set_feedback(
             "BMI Calculator",
             client.build_feedback_data()
             .add_number_field("Age", required=True, min=0, max=100)
@@ -35,7 +35,7 @@ async def on_response(response):
             / (float(response_fields[1]) * 12 + float(response_fields[2])) ** 2
         )
     else:
-        feedback = response.set_feedback_with_data(
+        feedback = await response.set_feedback_with_data(
             '{"type":"multiStep","steps":[{"name":"","action":{"type":"blocks","blocks":[{"type":"numberField","title":"Age","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true},{"type":"multipleChoiceField","title":"Gender","choices":[{"title":"Male "},{"title":"Female"}],"required":true},{"type":"numberField","title":"Height in cm","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true},{"type":"numberField","title":"Weight in kg","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true}]}}]}'
         )
 
@@ -51,10 +51,10 @@ async def on_response(response):
     if not os.path.exists("images"):
         os.mkdir("images")
     plt.savefig("images/fig1.png")
-    image = client.create_image("images/fig1.png")
+    image = await client.create_image("images/fig1.png")
 
     bmi_info = get_info_text(bmi)
-    feedback2 = response_feedback1.set_feedback(
+    feedback2 = await response_feedback1.set_feedback(
         "BMI Calculator",
         client.build_feedback_data()
         .add_text("Your BMI is: " + str(bmi_rounded) + bmi_info + "\n![](" + image.url + ")")
@@ -62,7 +62,7 @@ async def on_response(response):
     )
 
     response2 = await feedback2.wait_for_response()
-    response2.set_no_feedback()
+    await response2.set_no_feedback()
 
 
 client.on_response(on_response)
