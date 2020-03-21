ad50085e4662ecf47dd73ea753e530872d24a445
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2020-03-21 11:54:20 +0300

    feat: upgrade obschart to 0.13.0

--- obschartapp/__main__.py
+++ obschartapp/__main__.py
@@ -21,27 +21,26 @@ async def on_response(response: ProgramTrackActionResponse):
             "BMI Calculator",
             client.build_feedback_data()
             .add_number_field("Age", required=True, min=0, max=100)
-            .add_number_field("Height (feet)", required=True, min=0, max=100)
-            .add_number_field("Height (inches)", required=True, min=0, max=100)
-            .add_number_field("Weight (pounds)", required=True, min=0, max=100),
+            .add_number_field("Height (feet)", required=True, min=0, max=100, id="height_feet")
+            .add_number_field("Height (inches)", required=True, min=0, max=100, id="height_inches")
+            .add_number_field("Weight (pounds)", required=True, min=0, max=100, id="weight_pounds"),
         )
 
-        response_feedback1 = await feedback.wait_for_response()
-
-        response_fields = response_feedback1._data["response"]["steps"][0]["fields"]
+        feedback_response = await feedback.wait_for_response()
+        values = feedback_response.values
 
         bmi = 703 * (
-            float(response_fields[3])
-            / (float(response_fields[1]) * 12 + float(response_fields[2])) ** 2
+            float(values["weight_pounds"])
+            / (float(values["height_feet"]) * 12 + float(values["height_inches"])) ** 2
         )
     else:
         feedback = await response.set_feedback_with_data(
             '{"type":"multiStep","steps":[{"name":"","action":{"type":"blocks","blocks":[{"type":"numberField","title":"Age","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true},{"type":"multipleChoiceField","title":"Gender","choices":[{"title":"Male "},{"title":"Female"}],"required":true},{"type":"numberField","title":"Height in cm","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true},{"type":"numberField","title":"Weight in kg","min":-9007199254740991,"max":9007199254740991,"step":1,"required":true}]}}]}'
         )
 
-        response_feedback1 = await feedback.wait_for_response()
+        feedback_response = await feedback.wait_for_response()
 
-        response_fields = response_feedback1._data["response"]["steps"][0]["fields"]
+        response_fields = feedback_response.data["steps"][0]["fields"]
 
         bmi = float(response_fields[3]) / (float(response_fields[2]) / 100) ** 2
 
@@ -52,7 +51,7 @@ async def on_response(response: ProgramTrackActionResponse):
     image = await client.create_image(figure)
 
     bmi_info = get_info_text(bmi)
-    feedback2 = await response_feedback1.set_feedback(
+    feedback2 = await feedback_response.set_feedback(
         "BMI Calculator",
         client.build_feedback_data().add_text(
             "Your BMI is: " + str(bmi_rounded) + bmi_info + "\n![](" + image.url + ")"
